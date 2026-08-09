bbaeaecf45ac822846f3e0baf383c4f9d2bfdd31
Roshan-123456 <roshan9664087477d@gmail.com>
2026-08-09 18:29:24 +0530

    Fix AppComponent render test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,19 +17,23 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'test1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('test1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test1 app is running!');
+  
+    const compiled = fixture.nativeElement as HTMLElement;
+  
+    expect(compiled.querySelector('h2')?.textContent).toContain('Hello World');
   });
 });
