baf5676df1cc46733aed19c0013443f7f2618232
Roshan-123456 <roshan9664087477d@gmail.com>
2026-08-09 22:34:20 +0530

    Fix Node version for Angular CI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test1';
+  publicKey:any;
+  constructor(){
+    this.publicKey= environment.publicKey;
+    console.log(environment.publicKey);
+
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  publicKey: '__PUBLIC_KEY__'
 };
